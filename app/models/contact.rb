7c4a5de2dbe05c525cba89d6f5dc1605674905be
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-09-28 04:31:40 +0000

    test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,7 @@
 class Contact
   include Mongoid::Document
   has_one :test_contact_basics, dependent: :destroy
-  accepts_nested_attributes_for :test_contact_basics
   has_one :test_contact_details, dependent: :destroy
-  accepts_nested_attributes_for :test_contact_details
   has_one :tcm_basic_contact, dependent: :destroy
   has_one :tcm_patient_info, dependent: :destroy
 end
