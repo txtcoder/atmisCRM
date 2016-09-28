8130b813884c358eaf05c54f86ff60fd11e030a6
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-09-28 04:43:29 +0000

    test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact
   include Mongoid::Document
-  has_one :test_contact_basics, dependent: :destroy, required: true
-  has_one :test_contact_details, dependent: :destroy, required: true
+  has_one :test_contact_basics, dependent: :destroy
+  has_one :test_contact_details, dependent: :destroy
   has_one :tcm_basic_contact, dependent: :destroy
   has_one :tcm_patient_info, dependent: :destroy
 end
