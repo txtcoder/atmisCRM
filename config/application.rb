7fdca7cdc52ae5de30e24d6a96500e8e15596954
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-10-18 22:42:36 +0000

    x

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module AtmisCRM
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     Mongoid.load!('./config/mongoid.yml')
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origin '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch"]
+      end
+    end
   end
 end
