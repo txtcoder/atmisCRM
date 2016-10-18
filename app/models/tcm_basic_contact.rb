016608e2539a2ec255be1ee21683cbd96f430caa
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-10-18 23:40:10 +0000

    x

--- app/models/tcm_basic_contact.rb
+++ app/models/tcm_basic_contact.rb
@@ -3,8 +3,15 @@ class TcmBasicContact
   has_one :contact
   field :name, type: String
   field :picture, type: String
-  embeds_one :address, as: :addressable
-  embeds_one :phone, as: :phonable
+  field :address, type: String
+  field :postal_code, type: String
+  field :city, type: String
+  field :province, type:String
+  field :home_phone, type:String
+  field :cell_phone, type:String
+  field :work_phone, type:String
+  field :preferred_contact, type:String
+  field :referral, type:String
   field :email, type:String
   field :weight, type:Integer
   field :height, type:Integer
