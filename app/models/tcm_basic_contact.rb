6ebe88d5203a429504fe79484ffb3330b59e3e32
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-10-19 00:00:37 +0000

    x

--- app/models/tcm_basic_contact.rb
+++ app/models/tcm_basic_contact.rb
@@ -4,6 +4,7 @@ class TcmBasicContact
   field :name, type: String
   field :picture, type: String
   field :address, type: String
+  field :birth_date, type: Date
   field :postal_code, type: String
   field :city, type: String
   field :province, type:String
@@ -15,6 +16,5 @@ class TcmBasicContact
   field :email, type:String
   field :weight, type:Integer
   field :height, type:Integer
-  field :date_of_birth, type:Date
   field :occupation, type:String
 end
